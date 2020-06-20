edd499c7fcdc264279bb0fda8aea0ea35267b27a
lyonalyo <e.shterenberg@gmail.com>
2020-06-20 19:00:13 +0300

    Fetch

--- src/panels/MasterProfile.js
+++ src/panels/MasterProfile.js
@@ -1,89 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import bridge from '@vkontakte/vk-bridge';
 import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
 import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
-import Button from '@vkontakte/vkui/dist/components/Button/Button';
-import Group from '@vkontakte/vkui/dist/components/Group/Group';
-import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
-import {CellButton, Header, SimpleCell, Link, InfoRow, CardGrid, Card} from '@vkontakte/vkui';
+import {Button, Group, Header, Gradient, SimpleCell, InfoRow, CardScroll, Card, RichCell, Div} from '@vkontakte/vkui';
 import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton";
 import {IOS, platform} from "@vkontakte/vkui";
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
 import Icon24Back from '@vkontakte/icons/dist/24/back';
-import Icon24Add from '@vkontakte/icons/dist/24/add';
-import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton";
+import Icon28MessageOutline from '@vkontakte/icons/dist/28/message_outline';
 
-import logo from "../img/logo.png";
+import logo from "../img/logosmall.png";
 import './logoheader.css';
-
-
+import CardGrid from "@vkontakte/vkui/dist/es6/components/CardGrid/CardGrid";
 
 const osName = platform();
 
-
-const MasterProfile = ({ id, go, fetchedUser }) => (
+const Market = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
 		<PanelHeader
 			left={<PanelHeaderButton onClick={go} data-to="home">
 				{osName === IOS ? <Icon28ChevronBack/> : <Icon24Back/>}
 			</PanelHeaderButton>}
 		><img className="LogoHeader" src={logo} alt="RE-DO-ME"/></PanelHeader>
-		{fetchedUser &&
-		<Group title="User Data Fetched with VK Bridge">
-			<Cell
-				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
-				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
-			>
-				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
-			</Cell>
-		</Group>}
+
 		<Group>
-			<Header aside={<Link>Редактировать</Link>}>
-				Информация
-			</Header>
-			<SimpleCell multiline>
-				<InfoRow header="Специализация">
-					парикмахер-стилист
-				</InfoRow>
-			</SimpleCell>
-			<SimpleCell>
-				<InfoRow header="Место работы">
-					Салон InStyle, ул. Московская, д.52
-				</InfoRow>
-			</SimpleCell>
-			<SimpleCell>
-				<InfoRow header="Контактный телефон">
-					+7(906)578-87-96
-				</InfoRow>
-			</SimpleCell>
-			<SimpleCell>
-				<InfoRow header="Сертификаты">
-					123
-				</InfoRow>
-			</SimpleCell>
+			<Header separator="hide" mode="secondary">Запросы клиентов</Header>
+			<Group>
+				<SimpleCell before={<Avatar size={40} src={'https://sun1-99.userapi.com/TbZt2f5zjLR84J9f23JRSqbR3-GL2O6OQhaL_Q/04ICGL5RVKE.jpg?ava=1'} />}description="Хочу бороду, как у Райан Рейнольдс!" after={<Icon28MessageOutline />}>Василий Иванов</SimpleCell>
+				<CardGrid>
+					<Card size="m">
+						<div style={{ height: 96 }} />
+					</Card>
+					<Card size="m">
+						<div style={{ height: 96 }} />
+					</Card>
+				</CardGrid>
+				<Div>
+					<Button size="xl" level="2" onClick={go} data-to="marketchose">
+						Откликнуться
+					</Button>
+				</Div>
+			</Group>
+			<Group>
+				<SimpleCell before={<Avatar size={40} src={'https://sun9-71.userapi.com/c629525/v629525641/33e4c/lA6AIphGLDo.jpg?ava=1'} />}description="Хочу цвет волос, как у Клэр Буше!" after={<Icon28MessageOutline />}>Валерия Полухина</SimpleCell>
+				<CardGrid>
+					<Card size="m">
+						<div style={{ height: 96 }} />
+					</Card>
+					<Card size="m">
+						<div style={{ height: 96 }} />
+					</Card>
+				</CardGrid>
+				<Div>
+						<Button size="xl" level="2" onClick={go} data-to="marketchose">
+							Откликнуться
+						</Button>
+				</Div>
+			</Group>
 		</Group>
-
-		<Group separator="hide" header={<Header mode="secondary">Портфолио</Header>}>
-			<CardGrid>
-				<Card size="s">
-					<div style={{ height: 96 }} />
-				</Card>
-				<Card size="s">
-					<div style={{ height: 96 }} />
-				</Card>
-				<Card size="s">
-					<div style={{ height: 96 }} />
-				</Card>
-			</CardGrid>
-			<CellButton before={<Icon24Add />}>Добавить работу</CellButton>
-		</Group>
-
 	</Panel>
 );
 
 
 
-MasterProfile.propTypes = {
+Market.propTypes = {
 	id: PropTypes.string.isRequired,
 	go: PropTypes.func.isRequired,
 	fetchedUser: PropTypes.shape({
@@ -93,7 +75,7 @@ MasterProfile.propTypes = {
 		city: PropTypes.shape({
 			title: PropTypes.string,
 		}),
-	})
+	}),
 };
 
-export default MasterProfile;
+export default Market;
